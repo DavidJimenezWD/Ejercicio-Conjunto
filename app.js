1c3eff295ec22984996bfda151547fd7d5ae6b3e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-22 23:24:48 +0100

    Andadida segunda ruta con parametro y error

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const almacen = require("./almacen");
 
-
 const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 
-
+// Rutas
 app.get("/almacen", (req, res) => {
     res.send(almacen);
 });
 
 app.get("/almacen/:seccion", (req, res) => {
+
     const seccion = req.params.seccion;
-    res.send(almacen[seccion]);
+
+    // Si el parametro es una de las secciones del almacen
+    if ( Object.keys(almacen).includes(seccion) ) {
+        res.send(almacen[seccion]);
+    } else {
+        res.status(404).send("No se ha podido encontrar");
+    }
+
 });
 
 
