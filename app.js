46d583d546131b8de0b41b852ac421f98f327ff3
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-23 00:05:48 +0100

    Ruta post completada

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const almacen = require("./almacen");
+const script = require("./script");
+
 
 const express = require("express");
 const app = express();
 
+
 app.use(express.static("public"));
 app.use(express.json());
+
 
 // Rutas
 app.get("/almacen", (req, res) => {
@@ -24,5 +28,52 @@ app.get("/almacen/:seccion", (req, res) => {
 
 });
 
+app.post("/almacen/:seccion", (req, res) => {
+
+    const seccion = req.params.seccion;
+
+    // Si el parametro es una de las secciones del almacen
+    if ( Object.keys(almacen).includes(seccion) ) {
+
+        const nuevoArticulo = script.creaObjetoNuevoArticulo(req, almacen[seccion]);
+        const listaNombresArticulos = script.creaListaNombresArticulos(almacen[seccion]);
+
+        // Si el objeto no esta en el almacen todavia
+        if ( !listaNombresArticulos.includes(nuevoArticulo.nombre) ) {
+            almacen[seccion].push(nuevoArticulo);
+            res.send( { msg: "Articulo añadido"});
+        } else {
+            res.status(422).send("El articulo existe ya");
+        }
+   
+    } else {
+        res.status(404).send("No se ha podido encontrar");
+    }
+});
+
+app.put("/almacen/:seccion/:nombre", (req, res) => {
+
+    const seccion = req.params.seccion;
+    const nombre = req.params.nombre;
+    const listaNombresArticulos = script.creaListaNombresArticulos(almacen[seccion]);
+
+    // Si el paramatro seccion es una de las secciones del almacen
+    if ( Object.keys(almacen).includes(seccion) ) {
+
+        const listaNombresArticulos = script.creaListaNombresArticulos(almacen[seccion]);
+
+        // Si el articulo esta efectivamente en el almacen
+        if ( listaNombresArticulos.includes(nuevoArticulo.nombre) ) {
+            almacen[seccion].push(nuevoArticulo);
+            res.send( { msg: "Articulo añadido"});
+        } else {
+            res.status(404).send("El articulo no existe");
+        
+        }
+   
+    } else {
+        res.status(404).send("No se ha podido encontrar");
+    }
+});
 
 app.listen(3000);
---
+++ script.js
@@ -0,0 +1,39 @@
+const almacen = require("./almacen");
+
+function creaObjetoNuevoArticulo(req, arr) {
+
+    const nombre = req.body.nombre;
+    const descripccion = req.body.descripccion;
+    const img = req.body.img;
+    const precio = req.body.img;
+
+    const nuevoObjeto = {
+        nombre,
+        descripccion,
+        img,
+        precio
+    }
+
+    return nuevoObjeto;
+}
+
+
+function creaListaNombresArticulos(arr) {
+    let listaNombresArticulos = [];
+
+    arr.forEach( obj => {
+        listaNombresArticulos.push( obj.nombre );
+    });
+
+    return listaNombresArticulos;    
+}
+
+
+function encuentraArticuloConNombre(nombre) {
+    for ( let seccion of almacen ) {
+        seccion.for
+    }
+}
+
+exports.creaObjetoNuevoArticulo = creaObjetoNuevoArticulo;
+exports.creaListaNombresArticulos = creaListaNombresArticulos;
