12e970ebe21876d0d2f54b780ab4c46baa75c03b
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-23 09:25:55 +0100

    Rutas anyadir y editar hechas

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/almacen/:seccion", (req, res) => {
 
 });
 
-app.post("/almacen/:seccion", (req, res) => {
+app.post("/almacen/anyadir/:seccion", (req, res) => {
 
     const seccion = req.params.seccion;
 
@@ -51,11 +51,13 @@ app.post("/almacen/:seccion", (req, res) => {
     }
 });
 
-app.put("/almacen/:seccion/:nombre", (req, res) => {
+app.put("/almacen/editar/:seccion/:nombre", (req, res) => {
 
     const seccion = req.params.seccion;
-    const nombre = req.params.nombre;
+    const nombreViejo = req.params.nombre.toUpperCase();
     const listaNombresArticulos = script.creaListaNombresArticulos(almacen[seccion]);
+    const nuevoArticulo = script.creaObjetoNuevoArticulo(req, almacen[seccion]);
+    
 
     // Si el paramatro seccion es una de las secciones del almacen
     if ( Object.keys(almacen).includes(seccion) ) {
@@ -63,13 +65,18 @@ app.put("/almacen/:seccion/:nombre", (req, res) => {
         const listaNombresArticulos = script.creaListaNombresArticulos(almacen[seccion]);
 
         // Si el articulo esta efectivamente en el almacen
-        if ( listaNombresArticulos.includes(nuevoArticulo.nombre) ) {
-        //     almacen[seccion].push(nuevoArticulo);
-        //     res.send( { msg: "Articulo añadido"});
-        // } else {
-        //     res.status(404).send("El articulo no existe");
-        
-        // }
+        if ( listaNombresArticulos.includes(nombreViejo) ) {
+            const IndexObjetoParaModificar = almacen[seccion].findIndex(obj => obj.nombre === nombreViejo);
+            
+            // Eliminar objeto para modificar y anadir el modificado
+            almacen[seccion].splice(IndexObjetoParaModificar, 1);
+            almacen[seccion].push(nuevoArticulo);
+
+            res.send( { msg: "Articulo modificado"});
+
+        } else {
+            res.status(404).send("El articulo no existe");
+        }
    
     } else {
         res.status(404).send("No se ha podido encontrar");
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const almacen = require("./almacen");
 
 function creaObjetoNuevoArticulo(req, arr) {
 
-    const nombre = req.body.nombre;
+    const nombre = req.body.nombre.toUpperCase();
     const descripccion = req.body.descripccion;
     const img = req.body.img;
     const precio = req.body.img;
