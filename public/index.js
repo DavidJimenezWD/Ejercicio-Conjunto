65227edf7671c7b3337477380fb97f84da0e3e5a
David Jimenez <davidjimenezwd@gmail.com>
2021-03-23 12:32:29 +0100

    Fetch buscar

--- public/index.js
+++ public/index.js
@@ -1,21 +1,48 @@
-fetch("/almacen").then(res=>res.json()).then(res=>{
+//Muestra todos los articulos en la index
 
-const arrays=Object.keys(res);
-for(let i=0;i<arrays.length;i++){
-    for(let j=0;j<res[arrays[i]].length;j++){
-      document.getElementById("div-articulos").innerHTML+=`
+fetch("/almacen")
+  .then((res) => res.json())
+  .then((res) => {
+    const arrays = Object.keys(res);
+    for (let i = 0; i < arrays.length; i++) {
+      for (let j = 0; j < res[arrays[i]].length; j++) {
+        document.getElementById("div-articulos").innerHTML += `
       <div class="articulo">
       <h2>${res[arrays[i]][j].nombre}</h2>
       <img src="${res[arrays[i]][j].img}" alt="foto"/>
       <p><span>Descripcción: </span>${res[arrays[i]][j].descripccion}</p>
       <h3>Precio: ${res[arrays[i]][j].precio}</h3>
       </div>`;
+      }
     }
-}
+  });
 
 
 
+// Aqui realizamos la busqueda atraves del input
+
+document.getElementById("btn-buscar").addEventListener("click", () => {
+  const seccion = document.getElementById("busqueda").value;
+  document.getElementById("mostrar").innerHTML="";
+  fetch(`/almacen/${seccion}`)
+    .then((res) => res.json())
+    .then((res) => {
+      const result=Object.values(res);
+
+      for(let i=0;i<result.length;i++){
+        document.getElementById("mostrar").innerHTML+=`
+        <div class="encontrados">
+        <h2>${result[i].nombre}</h2>
+        <img src="${result[i].img}" alt="foto"/>
+        <p><span>Descripcción: </span>${result[i].descripccion}</p>
+        <h3>Precio: ${result[i].precio}</h3>
+        </div>`;        
+
+      }
+
+      document.getElementById("div-articulos").innerHTML="";
+
+    });
 });
 
-
-
+//
